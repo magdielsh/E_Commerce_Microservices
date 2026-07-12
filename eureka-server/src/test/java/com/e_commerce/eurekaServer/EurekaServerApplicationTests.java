4f6eb46e59b32e92b44d72c248370d3d0c1ee285
Magdiel Santana Hernández <magdielsh30@gmail.com>
2026-07-12 16:10:28 +0200

    Prueba

--- eureka-server/src/test/java/com/e_commerce/eurekaServer/EurekaServerApplicationTests.java
+++ eureka-server/src/test/java/com/e_commerce/eurekaServer/EurekaServerApplicationTests.java
@@ -1,12 +1,9 @@
 package com.e_commerce.eurekaServer;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class EurekaServerApplicationTests {
 
-	@Test
 	void contextLoads() {
 	}
 
