1b85641fb1e08906e1011854df7f8ec1ffccd609
Magdiel Santana Hernández <magdielsh30@gmail.com>
2026-08-11 17:56:52 +0200

    Agregando MongoBD

---
+++ reviews-service/src/test/java/com/e_commerce/reviewsservice/ReviewsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e_commerce.reviewsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
